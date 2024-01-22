dc230ce09485192279e50d3d6a9ddd453459e78a
Justin Koo <jhtkoo@gmail.com>
2024-01-22 20:51:58 +0000

    Revert "Improved BaseAlgorithm.js code quality"
    This reverts commit 15f6c1ad3b6828f6e77f058eb645f123a0689772.

--- app/src/utilities/algorithms/BaseAlgorithm.js
+++ app/src/utilities/algorithms/BaseAlgorithm.js
@@ -1,3 +1,4 @@
+// A base class for implementing pathfinding algorithms.
 class BaseAlgorithm {
     constructor(stations) {
         this.stations = stations;
@@ -7,60 +8,60 @@ class BaseAlgorithm {
         this.visitedConnections = [];
     }
 
-    initializeDistances(startStation) {
-        for (const [stationName] of Object.entries(this.stations)) {
-            this.distances.set(stationName, Infinity);
-            this.visited.set(stationName, false);
-            this.previousStation.set(stationName, null);
-        }
-        this.distances.set(startStation, 0);
+    initializeDistances(startStationName) {
+        Object.keys(this.stations).forEach(stationName => {
+            this.distances[stationName] = Infinity;
+            this.visited[stationName] = false;
+            this.previousStation[stationName] = null;
+        });
+        this.distances[startStationName] = 0;
     }
 
     markStationAsVisited(stationName) {
-        this.visited.set(stationName, true);
+        this.visited[stationName] = true;
     }
-
+    
     markConnectionAsVisited(startStation, endStation) {
         this.visitedConnections.push({ start: startStation, end: endStation });
     }
 
     updateDistances(currentStation, neighborName, neighborDistance) {
-        const newDistance = this.distances.get(currentStation) + neighborDistance;
+        const newDistance = this.distances[currentStation] + neighborDistance;
 
-        if (newDistance < this.distances.get(neighborName)) {
-            this.distances.set(neighborName, newDistance);
-            this.previousStation.set(neighborName, currentStation);
+        if (newDistance < this.distances[neighborName]) {
+            this.distances[neighborName] = newDistance;
+            this.previousStation[neighborName] = currentStation;
             this.enqueue(neighborName, newDistance);
         }
-    }
-
-    runAlgorithm(startStation, endStation) {
-        const startTime = performance.now();
-        const result = this.searchPath(startStation, endStation);
-        const endTime = performance.now();
-        result['duration'] = endTime - startTime;
-        return result;
     }
 
     // Abstract methods to be implemented by subclasses
     enqueue(stationName, distance) {
         throw new Error("enqueue method must be implemented by subclasses, returning a boolean value");
     }
-
+    
     dequeue() {
         throw new Error("dequeue method must be implemented by subclasses, returning a boolean value");
     }
-
+    
     isEmpty() {
         throw new Error("isEmpty method must be implemented by subclasses, returning a boolean value");
     }
-
-    searchPath(startStation, endStation) {
+    
+    searchPath(startStationName, endStationName) {
         throw new Error("searchPath method must be implemented by subclasses, returning a boolean value");
     }
-
+    
     constructPath(previousStation, startStation, endStation) {
         throw new Error("constructPath method must be implemented by subclasses, returning a boolean value");
+    }
+
+    runAlgorithm(startStationName, endStationName) {
+        const startTime = performance.now();
+        const result = this.searchPath(startStationName, endStationName);
+        const endTime = performance.now();
+        result['duration'] = endTime - startTime;
+        return result;
     }
 }
 
