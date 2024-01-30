96704a62b94504a9e458eaac3b6eeecf72d6ffb5
Justin Koo <jhtkoo@gmail.com>
2024-01-30 16:34:57 +0000

    Added tests for SearchHandler

--- app/src/utilities/services/__tests__/DebuggerHandler.test.js
+++ app/src/utilities/services/__tests__/DebuggerHandler.test.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import DebuggerHandler from '../DebuggerHandler';
 
 describe('DebuggerHandler.js tests', () => {
@@ -8,7 +5,6 @@ describe('DebuggerHandler.js tests', () => {
     let debuggerHandler;
 
     beforeEach(() => {
-        // Create a mock App instance
         appInstance = {
             state: {
                 selectedStartStation: null,
@@ -17,39 +13,28 @@ describe('DebuggerHandler.js tests', () => {
             },
             setState: jest.fn(),
         };
-
-        // Create an instance of DebuggerHandler with the mock App instance
         debuggerHandler = new DebuggerHandler(appInstance);
     });
 
-    test('resetDebuggerState should reset the debugger state', () => {
-        // Call the resetDebuggerState method
+    test('resetDebuggerState() should reset the debugger state', () => {
         debuggerHandler.resetDebuggerState();
-
-        // Expect the setState method to be called with null debugger state
         expect(appInstance.setState).toHaveBeenCalledWith({ debugger: null });
     });
 
-    test('setDebuggerState should set the correct debugger state for missing fields', () => {
-        // Call the setDebuggerState method
+    test('setDebuggerState() should set the correct debugger state for missing fields', () => {
         debuggerHandler.setDebuggerState();
-
-        // Expect the setState method to be called with the appropriate debugger message
         expect(appInstance.setState).toHaveBeenCalledWith({
         debugger: 'The following fields are not selected: Start station, End station, Algorithm',
         });
     });
 
-    test('setDebuggerState should not set debugger state when all fields are selected', () => {
+    test('setDebuggerState() should not set debugger state when all fields are selected', () => {
         // Set values for all fields
         appInstance.state.selectedStartStation = 'Station A';
         appInstance.state.selectedEndStation = 'Station B';
         appInstance.state.selectedAlgorithm = 'Dijkstra';
       
-        // Call the setDebuggerState method
         debuggerHandler.setDebuggerState();
-      
-        // Expect the setState method not to be called
         expect(appInstance.setState).not.toHaveBeenCalled();
       });
       
---
+++ app/src/utilities/services/__tests__/SearchHandler.test.js
@@ -0,0 +1,54 @@
+import SearchHandler from '../SearchHandler';
+
+
+describe('SearchHandler.js tests', () => {
+    let appInstanceMock;
+    let algorithmSearchServiceMock;
+
+    beforeEach(() => {
+        algorithmSearchServiceMock = {
+            search: jest.fn(() => Promise.resolve({
+                Dijkstra: {
+                    distance: 10,
+                    path: ['Station1', 'Station2'],
+                    duration: 5,
+                },
+            })),
+        };
+
+        appInstanceMock = {
+            state: {
+                selectedStartStation: 'StartStation',
+                selectedEndStation: 'EndStation',
+                selectedAlgorithm: 'Dijkstra',
+            },
+            resetStates: jest.fn(),
+            setAlgorithmResultState: jest.fn(),
+            mapCanvas: {
+                renderAlgorithmSearchResults: jest.fn(),
+                moveViewerToStation: jest.fn(),
+            },
+            algorithmSearchService: algorithmSearchServiceMock,
+            setDebuggerState: jest.fn(),
+        };
+    });
+
+    test('should call search and update states on handleSearchClick()', async () => {
+        const searchHandler = new SearchHandler(appInstanceMock);
+        await searchHandler.handleSearchClick();
+
+        expect(appInstanceMock.resetStates).toHaveBeenCalled();
+        expect(algorithmSearchServiceMock.search).toHaveBeenCalledWith('StartStation', 'EndStation', 'Dijkstra');
+        expect(appInstanceMock.setAlgorithmResultState).toHaveBeenCalledWith(['Station1', 'Station2'], 10, 5);
+        expect(appInstanceMock.mapCanvas.renderAlgorithmSearchResults).toHaveBeenCalled();
+        expect(appInstanceMock.mapCanvas.moveViewerToStation).toHaveBeenCalled();
+        expect(appInstanceMock.setDebuggerState).not.toHaveBeenCalled();
+    });
+
+    test('should call setDebuggerState() if not all fields are selected', async () => {
+        appInstanceMock.state.selectedAlgorithm = null;
+        const searchHandler = new SearchHandler(appInstanceMock);
+        await searchHandler.handleSearchClick();
+        expect(appInstanceMock.setDebuggerState).toHaveBeenCalled();
+    });
+});
