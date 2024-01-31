9a5c7f7f014faae354d6807d4e465c2ec560e4ba
Justin Koo <jhtkoo@gmail.com>
2024-01-31 15:04:56 +0000

    Added exclusion to jest test files and updated MapGraph tests

--- app/src/utilities/map_assets/__tests__/MapGraph.test.js
+++ app/src/utilities/map_assets/__tests__/MapGraph.test.js
@@ -13,38 +13,49 @@ describe('MapGraph.js tests', () => {
     });
 
     test('should maintain bidirectional connection between stations', () => {
-        mapGraph.addNeighbourToStation('StationA', 'StationB', 10.5);
+        mapGraph.addNeighbourToStation('stationA', 'stationB', 10.5);
 
         // Check if the source and neighbor connection is added correctly
         expect(mapGraph.adjacencyList.size).toBe(2);
-        expect(mapGraph.adjacencyList.get('StationA').size).toBe(1);
-        expect(mapGraph.adjacencyList.get('StationA').get('StationB')).toBe(10.5);
+        expect(mapGraph.adjacencyList.get('stationA').size).toBe(1);
+        expect(mapGraph.adjacencyList.get('stationA').get('stationB')).toBe(10.5);
 
         // Check if the corresponding neighbor-source connection is added correctly
-        expect(mapGraph.adjacencyList.get('StationB').size).toBe(1);
-        expect(mapGraph.adjacencyList.get('StationB').get('StationA')).toBe(10.5);
+        expect(mapGraph.adjacencyList.get('stationB').size).toBe(1);
+        expect(mapGraph.adjacencyList.get('stationB').get('stationA')).toBe(10.5);
     });
 
     test('should handle multiple neighbors for a station', () => {
-        mapGraph.addNeighbourToStation('StationA', 'StationB', 10.5);
-        mapGraph.addNeighbourToStation('StationA', 'StationC', 15.0);
+        mapGraph.addNeighbourToStation('stationA', 'stationB', 10.5);
+        mapGraph.addNeighbourToStation('stationA', 'StationC', 15.0);
 
         // Check if all neighbors are added correctly
         expect(mapGraph.adjacencyList.size).toBe(3);
-        expect(mapGraph.adjacencyList.get('StationA').size).toBe(2);
-        expect(mapGraph.adjacencyList.get('StationA').get('StationB')).toBe(10.5);
-        expect(mapGraph.adjacencyList.get('StationA').get('StationC')).toBe(15.0);
+        expect(mapGraph.adjacencyList.get('stationA').size).toBe(2);
+        expect(mapGraph.adjacencyList.get('stationA').get('stationB')).toBe(10.5);
+        expect(mapGraph.adjacencyList.get('stationA').get('StationC')).toBe(15.0);
 
-        // Check bidirectional connection for StationB
-        expect(mapGraph.adjacencyList.get('StationB').size).toBe(1);
-        expect(mapGraph.adjacencyList.get('StationB').get('StationA')).toBe(10.5);
+        // Check bidirectional connection for stationB
+        expect(mapGraph.adjacencyList.get('stationB').size).toBe(1);
+        expect(mapGraph.adjacencyList.get('stationB').get('stationA')).toBe(10.5);
     });
 
     test('should update edge distance', () => {
-        mapGraph.addNeighbourToStation('StationA', 'StationB', 10.5);
-        mapGraph.addNeighbourToStation('StationA', 'StationB', 8);
+        mapGraph.addNeighbourToStation('stationA', 'stationB', 10.5);
+        mapGraph.addNeighbourToStation('stationA', 'stationB', 8);
 
-        expect(mapGraph.adjacencyList.get('StationA').size).toBe(1);
-        expect(mapGraph.adjacencyList.get('StationA').get('StationB')).toBe(8);
+        expect(mapGraph.adjacencyList.get('stationA').size).toBe(1);
+        expect(mapGraph.adjacencyList.get('stationA').get('stationB')).toBe(8);
     })
+
+    test('getNeighbourDistance() should return distance if source station in adjacency list', () => {
+        mapGraph.addNeighbourToStation('stationA', 'stationB', 10.5);
+        const result = mapGraph.getNeighbourDistance('stationA', 'stationB');
+        expect(result).toBe(10.5);
+    });    
+
+    test('getNeighbourDistance() should return undefined if source station not in adjacency list', () => {
+        const result = mapGraph.getNeighbourDistance('nonExistentStation', 'stationB');
+        expect(result).toBeUndefined();
+    });    
 });
